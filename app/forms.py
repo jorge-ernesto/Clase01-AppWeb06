797e36ef7c04058e4c2c05929e065d666e45aef1
Ernesto <jorge.cywdt@gmail.com>
2023-04-14 19:28:29 -0500

    Notas para entender mejor el codigo

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
 class AgregarTarea(forms.Form):
-    tarea=forms.CharField()
-    
+    # Definir los datos de la clase (inputs del formulario)
+    # Si no se especifica lo contrario, todos los inputs seran required=TRUE por defecto
+    tarea = forms.CharField()
